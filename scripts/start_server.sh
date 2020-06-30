89448cf2e9a30c5644584ea788598c52f273ab83
paulo@ubots.com.br <Paulinho01@>
2020-06-30 14:11:37 -0300

    ajustando script de start

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,6 +6,6 @@ VIRTUALENV=/home/ubuntu/venv
 pushd $(dirname "$0")
 source $VIRTUALENV/bin/activate
 export FLASK_APP=../app.py
-flask run --host=0.0.0.0 --port 8080
+flask run --host=0.0.0.0 --port 8080 >> log.txt 2>&1 &
 deactivate
 popd
