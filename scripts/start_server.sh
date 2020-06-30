37d62156646e7e01d469105a04e5938e9e576f89
paulo@ubots.com.br <Paulinho01@>
2020-06-30 14:21:05 -0300

    arrumando logs

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Configurar o caminho para o Virtualenv
-VIRTUALENV=/home/ubuntu/venv
+VIRTUALENV=/home/paulo/venv
 
 pushd $(dirname "$0")
 source $VIRTUALENV/bin/activate
 export FLASK_APP=../app.py
-flask run --host=0.0.0.0 --port 8080 >> log.txt 2>&1 &
+flask run --host=0.0.0.0 --port 8080 >> ../logs/log.txt 2>&1 &
 deactivate
 popd
